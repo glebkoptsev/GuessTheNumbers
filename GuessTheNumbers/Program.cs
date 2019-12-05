227f7899e644c4792ee224c6d5291bd2472a2090
Koptsev G.A <44699659+ImVeriBed@users.noreply.github.com>
2019-12-05 22:43:55 +0300

    сценарий человека окончен

--- GuessTheNumbers/Program.cs
+++ GuessTheNumbers/Program.cs
@@ -10,6 +10,55 @@ namespace GuessTheNumbers
     {
         static void Main(string[] args)
         {
+        start:
+            Console.WriteLine("Введите количество отгадываемых чисел");
+            string cntstr = Console.ReadLine();
+            if (!int.TryParse(cntstr, out int cnt)) goto start;
+            var compNumbers = new List<int>();
+
+            var rnd = new Random();
+            for (int i = 0; i < cnt; i++) compNumbers.Add(rnd.Next(0, 9));
+
+            Console.WriteLine("Компьютер загадал числа");
+            Console.WriteLine("Ваша очередь загадывать!");
+            var humNumbers = new List<int>();
+
+            for (int i = 0; i < cnt; i++)
+            {
+            error:
+                Console.WriteLine($"Введите число номер {i + 1}");
+                string numbstr = Console.ReadLine();
+                if (!int.TryParse(numbstr, out int numb)) goto error;
+                humNumbers.Add(numb);
+            }
+
+            human:
+            Console.WriteLine($"Ваш ход, введите {cnt} чисел");
+
+            var humAnswer = new List<int>();
+            for (int i = 0; i < cnt; i++)
+            {
+            error:
+                Console.WriteLine($"Введите число номер {i + 1}");
+                string numbstr = Console.ReadLine();
+                if (!int.TryParse(numbstr, out int numb)) goto error;
+                humAnswer.Add(numb);
+            }
+            var failCount = compNumbers.Except(humAnswer).Count();
+            if (failCount == 0)
+            {
+                Console.WriteLine("Победил человек!");
+                goto end;
+            }
+
+            if (Convert.ToDouble(failCount) / Convert.ToDouble(cnt) > 0.5)
+            {
+                Console.WriteLine("Было близко, но нет");
+                goto human;
+            }
+
+        end:
+            Console.Read();
         }
     }
 }
